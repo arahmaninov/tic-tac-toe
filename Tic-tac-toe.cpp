05058fdf99fddf79806f78e3bc97b919de707dfa
Anton Rahmaninov <95240106+arahmaninov@users.noreply.github.com>
2023-08-30 13:02:45 +0300

    add win condition check

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -70,8 +70,26 @@ public:
 
     void checkWinCondition()
     {
-        if(board.cells[0] == PLAYER && board.cells[1] == PLAYER && board.cells[2] == PLAYER) // 1st row
-            isGameRunning = false, hasPlayerWon = true;
+        if((board.cells[0] == PLAYER && board.cells[1] == PLAYER && board.cells[2] == PLAYER) || // 1st row
+           (board.cells[3] == PLAYER && board.cells[4] == PLAYER && board.cells[5] == PLAYER) || // 2nd row
+           (board.cells[6] == PLAYER && board.cells[7] == PLAYER && board.cells[8] == PLAYER) || // 3d row
+           (board.cells[0] == PLAYER && board.cells[3] == PLAYER && board.cells[6] == PLAYER) || // 1st column
+           (board.cells[1] == PLAYER && board.cells[4] == PLAYER && board.cells[7] == PLAYER) || // 2nd column
+           (board.cells[2] == PLAYER && board.cells[5] == PLAYER && board.cells[8] == PLAYER) || // 3d column
+           (board.cells[0] == PLAYER && board.cells[4] == PLAYER && board.cells[8] == PLAYER) || // 1st diagonal
+           (board.cells[2] == PLAYER && board.cells[4] == PLAYER && board.cells[6] == PLAYER)    // 2nd diagonal
+           )
+            hasPlayerWon = true, isGameRunning = false;
+        else if((board.cells[0] == BOT && board.cells[1] == BOT && board.cells[2] == BOT) || // 1st row
+           (board.cells[3] == BOT && board.cells[4] == BOT && board.cells[5] == BOT) || // 2nd row
+           (board.cells[6] == BOT && board.cells[7] == BOT && board.cells[8] == BOT) || // 3d row
+           (board.cells[0] == BOT && board.cells[3] == BOT && board.cells[6] == BOT) || // 1st column
+           (board.cells[1] == BOT && board.cells[4] == BOT && board.cells[7] == BOT) || // 2nd column
+           (board.cells[2] == BOT && board.cells[5] == BOT && board.cells[8] == BOT) || // 3d column
+           (board.cells[0] == BOT && board.cells[4] == BOT && board.cells[8] == BOT) || // 1st diagonal
+           (board.cells[2] == BOT && board.cells[4] == BOT && board.cells[6] == BOT)    // 2nd diagonal
+           )
+            hasBotWon = true, isGameRunning = false;
     }
 
     void play()
@@ -96,6 +114,21 @@ public:
             {
                 board.clearBoard();
                 hasPlayerWon = false;
+                isGameRunning = true;
+                play();
+            }
+            else
+                return;
+        }
+        else if(hasBotWon)
+        {
+            std::cout << "\nUnfortunately you lost :\(\nWant to play again? ";
+            std::string choice;
+            std::cin >> choice;
+            if(choice == "y" || choice == "Y")
+            {
+                board.clearBoard();
+                hasBotWon = false;
                 isGameRunning = true;
                 play();
             }
