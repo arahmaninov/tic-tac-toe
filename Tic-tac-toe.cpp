fc1160a721b2255ad580bfba2f9854ef28d80ae0
Anton Rahmaninov <95240106+arahmaninov@users.noreply.github.com>
2023-08-30 12:43:41 +0300

    add player input

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 enum {
     EMPTY,
@@ -12,11 +12,21 @@ public:
     int cells[9] = {EMPTY, EMPTY, EMPTY,
                     EMPTY, EMPTY, EMPTY,
                     EMPTY, EMPTY, EMPTY};
+
+    void clearBoard()
+    {
+        for(int i = 0; i < 9; i++)
+            cells[i] = EMPTY;
+    }
 };
 
 class Game{
 public:
     Board board;
+    bool hasPlayerWon = false;
+    bool hasBotWon = false;
+    bool isGameRunning = true;
+
     void printBoard()
     {
         for(int i = 0; i < 9; i++)
@@ -35,12 +45,86 @@ public:
         std::cout << std::endl;
     }
 
+    void takePlayerInput()
+    {
+        std::string playerChoice;
+        bool isChoiceLegit = false;
+        do{
+            std::cout << "\nChoose a free cell (type a number from 1 to 9): ";
+            std::cin >> playerChoice;
+
+            if(!isInputValid(playerChoice))
+                std::cout << "Invalid input. Type a number from 1 to 9.\n\n";
+            else if( stoi(playerChoice) < 1 || stoi(playerChoice) > 9)
+                std::cout << "Invalid input. Type a number from 1 to 9.\n\n";
+            else if(board.cells[stoi(playerChoice) - 1] != EMPTY)
+                std::cout << "This cell is already taken. Choose another cell.\n\n";
+            else if(board.cells[stoi(playerChoice) - 1] == EMPTY)
+            {
+                board.cells[stoi(playerChoice) - 1] = PLAYER;
+                isChoiceLegit = true;
+            }
+
+        }while(!isChoiceLegit);
+    }
+
+    void checkWinCondition()
+    {
+        if(board.cells[0] == PLAYER && board.cells[1] == PLAYER && board.cells[2] == PLAYER) // 1st row
+            isGameRunning = false, hasPlayerWon = true;
+    }
+
+    void play()
+    {
+        while(isGameRunning)
+        {
+            printBoard();
+            takePlayerInput();
+            checkWinCondition();
+        }
+        printResult();
+    }
+
+    void printResult()
+    {
+        if(hasPlayerWon)
+        {
+            std::cout << "\nCongratulations! You won!\nWant to play again? ";
+            std::string choice;
+            std::cin >> choice;
+            if(choice == "y" || choice == "Y")
+            {
+                board.clearBoard();
+                hasPlayerWon = false;
+                isGameRunning = true;
+                play();
+            }
+            else
+                return;
+        }
+
+    }
+
+    bool isInputValid(std::string input)
+    {
+        for(const auto& c : input)
+        {
+            if(!std::isdigit(c))
+                return false;
+        }
+
+        return true;
+    }
 };
+
+
 
 int main()
 {
     Game game;
-    game.printBoard();
+    game.play();
+
 
     return 0;
 }
+
